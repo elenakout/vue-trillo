63db5a208d9119dbc1e44d6e8af545e2aca9beb5
elenakout <elenakout@gmail.com>
2019-07-01 19:31:18 +0300

    download svgs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="main-header">
     <header class="header">
-      Header Part
+      <img src="../assets/img/logo.png" alt="trillo logo" class="logo" />
+      <form action="#" class="search">
+        <input type="text" class="search__input" placeholder="Search hotels" />
+        <button class="search__button"></button>
+      </form>
     </header>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {};
+  }
+};
 </script>
 
 <style lang="scss"></style>
