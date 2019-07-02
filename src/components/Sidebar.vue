8307577f2a15dd2828d81eaf174370f7f1718ff1
elenakout <elenakout@gmail.com>
2019-07-02 21:59:57 +0300

    078 Building the Navigation - Part 1

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,57 @@
 <template>
   <nav class="sidebar">
-    Navigation
+    <ul class="side-nav">
+      <li class="side-nav__item">
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="../assets/img/sprite.svg#icon-home"></use>
+          </svg>
+          <span>Hotel</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use
+              xlink:href="../assets/img/sprite.svg#icon-aircraft-take-off"
+            ></use>
+          </svg>
+          <span>Flight</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="../assets/img/sprite.svg#icon-key"></use>
+          </svg>
+          <span>Car rental</span>
+        </a>
+      </li>
+
+      <li>
+        <a href="#" class="side-nav__link">
+          <svg class="side-nav__icon">
+            <use xlink:href="../assets/img/sprite.svg#icon-map"></use>
+          </svg>
+          <span>Tours</span>
+        </a>
+      </li>
+    </ul>
+
+    <div class="legal">&copy; {{ year }} by trillo. All rights reserved.</div>
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear()
+    };
+  }
+};
 </script>
 
 <style lang="scss"></style>
